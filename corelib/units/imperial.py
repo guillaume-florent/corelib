cb82129c282c9308b6977b20da5ec2e785b75e7f
Guillaume Florent <guillaume.florent@tutanota.com>
2018-11-09 17:00:03 +0100

    Feet, inches, fraction of inch conversion

---
+++ corelib/units/imperial.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+
+r"""Parse and convert imperial distance units"""
+
+from fractions import Fraction
+
+from corelib.units.distance import convert
+
+
+def convert_imperial_str(value, to_unit):
+    r"""Convert a distance expressed in the imperial system
+    (feet, inches, fraction of an inch) into the specified unit
+    
+    Parameters
+    ----------
+    value : str
+        String representation of the imperial distance
+    to_unit : str
+        The desired (metric based) unit
+
+    """
+    split_on_inches = value.strip().split("''")
+    # print(split_on_inches)
+    feet = float(split_on_inches[0].split("'")[0])
+    inches_int = int(split_on_inches[0].split("'")[1])
+    if len(split_on_inches) == 2 and split_on_inches[1] != "":
+        inches_frac = float(Fraction(split_on_inches[1]))
+    else:
+        inches_frac = 0.
+    inches_float = inches_int + inches_frac
+
+    return convert_imperial(feet=feet, inches=inches_float, to_unit=to_unit)
+
+
+def convert_imperial(feet=0., inches=0., to_unit="mm"):
+    r"""Convert a distance expressed in the imperial system
+    (feet, inches, fraction of an inch) into the specified unit
+    
+    Parameters
+    ----------
+    feet : float
+        Feet
+    inches : float
+        Inches
+    to_unit : str
+        The desired (metric based) unit
+
+    """
+    return convert(feet, from_unit="feet", to_unit=to_unit) + convert(inches, from_unit="inches", to_unit=to_unit)
+
+
+if __name__ == "__main__":
+    print(convert_imperial(feet=1., inches=2.5, to_unit="mm"))
+    print(convert_imperial_str(value="1'2''1/2", to_unit="mm"))
+    print(convert_imperial(feet=1., inches=2, to_unit="mm"))
+    print(convert_imperial_str(value="1'2''", to_unit="mm"))
+    print(convert_imperial(feet=1., inches=2.625, to_unit="mm"))
+    print(convert_imperial_str(value="1'2''5/8", to_unit="mm"))
