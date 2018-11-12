df1b492c5293e5f910db25fb175a530743600b56
Guillaume Florent <guillaume.florent@tutanota.com>
2018-11-12 15:49:42 +0100

    Handle case of inches without feet in front

--- TODO.rst
+++ TODO.rst
@@ -1,3 +1,5 @@
+conda build -> BROKEN -> why?
+
 See if scipy.constants can be used for the units package
 
 Create releases and tarballs so that the conda package can be built using url and fn from tarball
--- corelib/units/imperial.py
+++ corelib/units/imperial.py
@@ -20,13 +20,26 @@ def convert_imperial_str(value, to_unit):
 
     """
     split_on_inches = value.strip().split("''")
-    # print(split_on_inches)
-    feet = float(split_on_inches[0].split("'")[0])
-    inches_int = int(split_on_inches[0].split("'")[1])
+    # print("split_on_inches : %s" % str(split_on_inches))
+    # print("split_on_inches[0].split(''') : %s" % str(split_on_inches[0].split("'")))
+    if len(split_on_inches[0].split("'")) == 2:
+        feet = float(split_on_inches[0].split("'")[0])
+    elif len(split_on_inches[0].split("'")) == 1:
+        feet = 0.
+    else:
+        raise ValueError
+
+    # print("feet : %f" % feet)
+    if "'" in split_on_inches[0]:
+        inches_int = int(split_on_inches[0].split("'")[1])
+    else:
+        inches_int = int(split_on_inches[0])
+    # print("inches_int : %s" % str(inches_int))
     if len(split_on_inches) == 2 and split_on_inches[1] != "":
         inches_frac = float(Fraction(split_on_inches[1]))
     else:
         inches_frac = 0.
+    # print("inches_frac : %s" % str(inches_frac))
     inches_float = inches_int + inches_frac
 
     return convert_imperial(feet=feet, inches=inches_float, to_unit=to_unit)
@@ -52,7 +65,15 @@ def convert_imperial(feet=0., inches=0., to_unit="mm"):
 if __name__ == "__main__":
     print(convert_imperial(feet=1., inches=2.5, to_unit="mm"))
     print(convert_imperial_str(value="1'2''1/2", to_unit="mm"))
+
     print(convert_imperial(feet=1., inches=2, to_unit="mm"))
     print(convert_imperial_str(value="1'2''", to_unit="mm"))
+
     print(convert_imperial(feet=1., inches=2.625, to_unit="mm"))
     print(convert_imperial_str(value="1'2''5/8", to_unit="mm"))
+
+    print(convert_imperial(feet=0., inches=1., to_unit="mm"))
+    print(convert_imperial_str(value="1''", to_unit="mm"))
+
+    print(convert_imperial(feet=0., inches=1.5, to_unit="mm"))
+    print(convert_imperial_str(value="1''1/2", to_unit="mm"))
